329f8df8b00e3dca1b8f2bbd6f45504c2eca2230
Sid Sriwastav <sriwastav@gmail.com>
2025-01-02 16:11:44 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header(props) {
   return (
     <header>
       <h1 className="text-gradient">
-        You have {todosLength} open {taskOrTasks}
+        You have {todosLength} {taskOrTasks}
       </h1>
     </header>
   );
